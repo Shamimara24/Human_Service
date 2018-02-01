eafc8b5bfe5e66abb3f169d529850e5d8556dc52
Shamimara24 <mayamesharif24@gmail.com>
2018-02-01 09:00:20 +0600

    Add files via upload

---
+++ userStudent.java
@@ -0,0 +1,29 @@
+// This is a general idea of what each student will have 
+// as their info available to their co-ordinators or the department.
+
+
+
+public class userStudent {
+	
+		//private string student
+			//username(get and set)
+			//password(get and set)
+			//firstname(get and set)
+			//lastname(get and set)
+			//id(get and set)
+			//email(get and set)
+			//password(get and set)
+	
+			//hours???   (get and set)
+			//admin????    (boolean true or false)
+	
+
+	
+	//representation of toString
+	
+			//return
+				//admin ???
+				//username
+				//password
+				//fullname
+}
