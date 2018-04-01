45c81a9ee6c5359146b9c1ea8c7c12ea41e6bbb9
datlas4 <datlas4@students.rowan.edu>
2018-04-01 15:55:47 -0400

    Add files via upload
    Added Calendar Functionality

--- connect.php
+++ connect.php
@@ -14,6 +14,7 @@ $wednesday2 = filter_input(INPUT_POST, 'wednesday2');
 $thursday2 = filter_input(INPUT_POST, 'thursday2');
 $friday2 = filter_input(INPUT_POST, 'friday2');
 $saturday2 = filter_input(INPUT_POST, 'saturday2');
+$week = filter_input(INPUT_POST, 'week');
 
 $servername = "localhost";!!!
 $username = "username";!!!
@@ -27,8 +28,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 else{
-$sql = "INSERT INTO !!!TABLE!!! (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday2, Monday2, Tuesday2, Wednesday2, Thursday2, Friday2, Saturday2)
-VALUES ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday2', 'Monday2', 'Tuesday2', 'Wednesday2', 'Thursday2', 'Friday2', 'Saturday2')";
+$sql = "INSERT INTO !!!TABLE!!! (Date, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday2, Monday2, Tuesday2, Wednesday2, Thursday2, Friday2, Saturday2)
+VALUES ('week', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday2', 'Monday2', 'Tuesday2', 'Wednesday2', 'Thursday2', 'Friday2', 'Saturday2')";
 
 
 
