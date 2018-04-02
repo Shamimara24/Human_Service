83fd425eae74ddabaa9ee07d34c385df783b46c2
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-04-01 22:15:57 -0400

    Working timesheet
    timesheet.php page that successfully creates a new timesheet in the DB.
    Total hours cell doesnt display properly
    The whole entire table isn't displayed properly

--- SeniorProject/timesheet.php
+++ SeniorProject/timesheet.php
@@ -1,281 +1,295 @@
 <?php
+
 // Connect to the database
+
 error_reporting(E_ALL ^ E_NOTICE);
+
 ini_set('display_errors', 1);
+
 include('connect.php');
+
 $dbh = ConnectDB();
+
+mysqli_query($dbh, "SET SESSION sql_mode = ''");
+
 session_start();
 
+
+
 $userid = $_SESSION['userid'];
+
 $username = $_SESSION['username'];
+
 ?>
+
+
+
 
 
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
+
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Timesheet</title>
-	<link rel="stylesheet" href="style.css">
-    <style>
-      form {
-        /* Just to center the form on the page */
-        margin: 0 auto;
-        width: 400px;
-        /* To see the outline of the form */
-        padding: 1em;
-        border: 1px solid #CCC;
-        border-radius: 1em;
-      }
-      form div + div {
-        margin-top: 1em;
-      }
-      label {
-        /* To make sure that all labels have the same size and are properly aligned */
-        display: inline-block;
-        width: 90px;
-        text-align: right;
-      }
-      input, textarea {
-        /* To make sure that all text fields have the same font settings By default, textareas have a monospace font */
-        font: 1em sans-serif;
-        /* To give the same size to all text fields */
-        width: 300px;
-        box-sizing: border-box; /* To harmonize the look & feel of text field border */
-        border: 1px solid #999;
-      }
-      input:focus, textarea:focus {
-        /* To give a little highlight on active elements */
-        border-color: #000;
-      }
-      textarea {
-        /* To properly align multiline text fields with their labels */
-        vertical-align: top;
-        /* To give enough room to type some text */
-        height: 5em;
-      }
-      .button {
-        /* To position the buttons to the same position of the text fields */
-        padding-left: 90px;
-        /* same size as the label elements */
-      }
-	  input[type=number]{
-    width: 50px;
-} 
-      button {
-        /* This extra margin represent roughly the same space as the space between the labels and their text fields */
-        margin-left: .5em;
-      }
-    </style>
+
+    <link rel="stylesheet" href="style.css">
+	
+	<?php
+if($_POST['submit']){
+		$sunday1 = $_POST['Sunday'];
+
+        $monday1 = $_POST['Monday'];
+
+        $tuesday1 = $_POST['Tuesday'];
+
+        $wednesday1 = $_POST['Wednesday'];
+
+        $thursday1 = $_POST['Thursday'];
+
+        $friday1 = $_POST['Friday'];
+
+        $saturday1 = $_POST['Saturday'];
+
+        $sunday2 = $_POST['Sunday2'];
+
+        $monday2 = $_POST['Monday2'];
+
+        $tuesday2 = $_POST['Tuesday2'];
+
+        $wednesday2 = $_POST['Wednesday2'];
+
+        $thursday1 = $_POST['Thursday2'];
+
+        $friday2 = $_POST['Friday2'];
+
+        $saturday2 = $_POST['Saturday2'];
+		
+		echo print_r($_POST);
+		
+		$insert =  "INSERT INTO timesheets (total_hours, status, sunday1, monday1, tuesday1, wednesday1, ";
+
+        $insert .= "thursday1, friday1, saturday1, sunday2, monday2, tuesday2, wednesday2, thursday2, ";
+
+        $insert .= "friday2, saturday2) VALUES ";
+
+        $insert .= "('40', 'Pending', '$sunday1', '$monday1', '$tuesday1', '$wednesday1', '$thursday1', '$friday1', '$saturday1', ";
+
+        $insert .= "'$sunday2', '$monday2', '$tuesday2', '$wednesday2', '$thursday2', '$friday2', '$saturday2')";
+
+
+
+        $insertquery = mysqli_query($dbh, $insert);
+		
+		  if(!$insertquery){
+
+                                        echo "Timesheet Query returned false." . mysqli_error($dbh) . "\n";
+
+                                        }else
+
+                                                echo "Timesheet Query worked!\n";
+
+
+       
+        }
+?>
+
 </head>
-
-
-
 <body>
+
     <div class="nav">
+
         <img src="https://www.prepsportswear.com/media/images/college_logos/300x300/2126241_mktg_logo.png" class="mainavatar">
+
             <label for="toggle">&#9776;</label>
+
         <input type="checkbox" id="toggle"/>
+
         <div class="menu">
+
                 <a href="dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-                <a href="timesheet.php"><i class="fas fa-clock"></i> Timesheet</a>
+
+                <a href="timesheets.php"><i class="fas fa-clock"></i> Timesheet</a>
+
                 <a href="connections.php"><i class="fas fa-users"></i> Connections</a>
+
                 <a href="profile.php"><i class="fas fa-user"></i> Profile</a>
+
                 <a href="login.php" style="float:right"><i class="fas fa-sign-out-alt"></i></a>
+
         </div>
+
     </div>
-        <div class="wrapper2">
-        <div class="box5">
-            <h2>Timesheet</h2>
+<body>
+<div style="width: 600px; text-align: left; padding: 15px;">
+<form name="Timesheet" method="post" action="./timesheet.php">
+<p><b>Timesheet</b></p>
 
-<form name="Timesheet">
-<div align="center">
-<p>Week of:
-<p>Month:
-<select>
-    <option value="January">January</option>
-    <option value="February">February</option>
-    <option value="March">March</option>
-    <option value="April">April</option>
-    <option value="May">May</option>
-    <option value="June">June</option>
-    <option value="July">July</option>
-    <option value="August">August</option>
-    <option value="September">September</option>
-    <option value="October">October</option>
-    <option value="November">November</option>
-    <option value="December">December</option>
-</select>
-
-
-Year:
-<select>
-    <option value="2017">2017</option>
-    <option value="2018">2018</option>
-   
-  <form action='./login.php' method='post'> 
-   
-</select>
+<fieldset>
+<legend> Week 1 </legend>
 <table width="800">
-    <div>
-		<td align="center" width="38">
-        <label for="name">Sunday:</label>
-        <input type="number" value="0" name="sunday1" /> 
-		</td>
-    </div>
-    <div>
-		<td align="center" width="38">
-        <label for="name">Monday:</label>
-        <input type="number" value="0" name="monday1" /> 
-		</td>
-    </div>
-    <div>
-		<td align="center" width="38">
-        <label for="name">Tuesday:</label>
-        <input type="number" value="0" name="tuesday1" /> 
-		</td>
-	</div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Wednesday:</label>
-        <input type="number" value="0" name="wednesday1" /> 
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Thursday:</label>
-        <input type="number" value="0" name="thursday1" /> 
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Friday:</label>
-        <input type="number" value="0" name="friday1" /> 
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Saturday:</label>
-        <input type="number" value="0" name="saturday1" /> 
-		</td>
-    </div>
-	</table>
-	<br>
-	
-	<table width="800">
-	 <div>
-		<td align="center" width="38">
-        <label for="name">Sunday:</label>
-        <input type="number" value="0" name="sunday2" /> 
-		</td>
-    </div>
-    <div>
-		<td align="center" width="38">
-        <label for="name">Monday:</label>
-        <input type="number" value="0" name="monday2" />
-		</td>
-    </div>
-    <div>
-		<td align="center" width="38">
-        <label for="name">Tuesday:</label>
-        <input type="number" value="0" name="tuesday2" />
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Wednesday:</label>
-        <input type="number" value="0" name="wednesday2" />
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Thursday:</label>
-        <input type="number" value="0" name="thursday2" /> 
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Friday:</label>
-        <input type="number" value="0" name="friday2" />
-		</td>
-    </div>
-	<div>
-		<td align="center" width="38">
-        <label for="name">Saturday:</label>
-        <input type="number" value="0" name="saturday2" /> 
-		</td>
-	</table>
-	<br>
-	
-	<input type="submit" name="submit" value="Submit Timesheet">
 
+<tr><th align="center" width="38">Day</th>
+<td align="center" width="38">Monday</td>
+<td align="center" width="38">Tuesday</td>
+<td align="center" width="38">Wednesday</td>
+<td align="center" width="38">Thursday</td>
+<td align="center" width="38">Friday</td>
+<td align="center" width="38">Saturday</td>
+<td align="center" width="38">Sunday</td>
+
+<tr><th align="center" width="38">Hours</th>
+
+<td align="center" width="38"><input type="number" name="Monday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Tuesday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')" onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Wednesday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Thursday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Friday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Saturday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Sunday" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+
+</td>
+</tr>
+</table>
+
+</fieldset>
+
+<fieldset>
+<legend> Week 2 </legend>
+<table width="800">
+
+<tr><th align="center" width="38">Day</th>
+<td align="center" width="38">Monday</td>
+<td align="center" width="38">Tuesday</td>
+<td align="center" width="38">Wednesday</td>
+<td align="center" width="38">Thursday</td>
+<td align="center" width="38">Friday</td>
+<td align="center" width="38">Saturday</td>
+<td align="center" width="38">Sunday</td>
+<td align="center" width="38"> Total Hours</td></tr>
+<tr><th align="center" width="38">Hours</th>
+
+<td align="center" width="38"><input type="number" name="Monday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Tuesday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')" onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Wednesday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Thursday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Friday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Saturday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="38"><input type="number" name="Sunday2" size="1" maxlength="1" value=""
+ onkeypress="return inputLimiter(event,'Numbers')"  onblur="calc()" min="0" max="8">
+</td>
+
+<td align="center" width="60"><input class="right" type="number" name="Total" readonly="readonly" size="5" value="">
+</td>
+</tr>
+</table>
+
+</fieldset>
+
+ <input type="submit" name="submit" value="Submit Timesheet">
 </form>
 
-<?php
+<script type="text/javascript" language="javascript">
 
-if(isset($_POST['submit']) && !empty($_POST['submit'])){
-	echo "test test test test test \n";
-	$sunday1 = $_POST['sunday1'];
-	$monday1 = $_POST['monday1'];
-	$tuesday1 = $_POST['tuesday1'];
-	$wednesday1 = $_POST['wednesday1'];
-	$thursday1 = $_POST['thursday1'];
-	$friday1 = $_POST['friday1'];
-	$saturday1 = $_POST['saturday1'];
-	$sunday2 = $_POST['sunday2'];
-	$monday2 = $_POST['monday2'];
-	$tuesday2 = $_POST['tuesday2'];
-	$wednesday2 = $_POST['wednesday2'];
-	$thursday1 = $_POST['thursday1'];
-	$friday2 = $_POST['friday2'];
-	$saturday2 = $_POST['saturday2'];
-	
-	$insert =  "INSERT INTO timesheets (total_hours, status, sunday1, monday1, tuesday1, wednesday1, "; 
-	$insert .= "thursday1, friday1, saturday1, sunday2, monday2, tuesday2, wednesday2, thursday2, ";
-	$insert .= "friday2, saturday2) VALUES ";
-	$insert .= "('40', 'Pending', '$sunday1', '$monday1', '$tuesday1', '$wednesday1', '$thursday1', '$friday1', '$saturday1', ";
-	$insert .= "'$sunday2', '$monday2', '$tuesday2', '$wednesday2', '$thursday2', '$friday2', '$saturday2')";
-	
-	$insertquery = mysqli_query($dbh, $insert);
-	if(!$insertquery){
-					echo "Timesheet Query returned false." . mysqli_error($dbh) . "\n";
-					}else
-						echo "Timesheet Query worked!\n";
-	
-	
-
-	$sql = "SELECT s.bannerid FROM students s JOIN ";
-	$sql .= "users u ON (s.userid = u.userid) WHERE ";
-	$sql .= "u.userid = $userid";
-	
-	$query = mysqli_query($dbh, $sql);
-	if(!$query){
-		echo "Select bannerid Query returned false." . mysqli_error($dbh) . "\n";
-					}else
-						echo "Select bannerid Query worked!\n";
-					$result = mysqli_fetch_assoc($query);
-					$dbbannerid = $row['bannerid'];
-	
-	$date = date("Y-m-d");
-	$insert2 = "INSERT INTO studenttimesheets (bannerid, timesheetsid, fieldsiteid, coordinatorid, lastmodified) ";
-	$insert2 .= "VALUES ('$dbbannerid', 'tsidPlaceholder', 'fsidPlaceholder', 'coidPlaceholder', '$date')";
-	
-	$insertquery2 = mysqli_query($dbh, $insert2);
-	if(!$insertquery2){
-		echo "studenttimesheets Insert Query returned false." . mysqli_error($dbh) . "\n";
-					}else
-						echo "studenttimesheets Insert Query worked!\n";
-	
-	
-	}
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 
+function calc(){
+  Monday = document.Timesheet.Monday.value;
+  Tuesday = document.Timesheet.Tuesday.value; 
+  Wednesday = document.Timesheet.Wednesday.value;
+  Thursday = document.Timesheet.Thursday.value;
+  Friday = document.Timesheet.Friday.value;
+  Saturday = document.Timesheet.Saturday.value;
+  Sunday = document.Timesheet.Sunday.value;
+  Monday2 = document.Timesheet.Monday2.value;
+  Tuesday2 = document.Timesheet.Tuesday2.value; 
+  Wednesday2 = document.Timesheet.Wednesday2.value;
+  Thursday2 = document.Timesheet.Thursday2.value;
+  Friday2 = document.Timesheet.Friday2.value;
+  Saturday2 = document.Timesheet.Saturday2.value;
+  Sunday2 = document.Timesheet.Sunday2.value;
+  var RptTime = (Monday*1) + (Tuesday*1) + (Wednesday*1) + (Thursday*1) + (Friday*1) + (Saturday*1) + (Sunday*1) + (Monday2*1) + (Tuesday2*1) + (Wednesday2*1) + (Thursday2*1) + (Friday2*1) + (Saturday2*1) + (Sunday2*1);
+  document.Timesheet.Total.value = RptTime.toFixed(4);
+  var Frac = 0;
+  var Full = parseInt(RptTime);
+  RptTimeFrac = RptTime - Full;
+  if (RptTimeFrac < 0.25) { Frac = 0; } 
+  else { if (RptTimeFrac < 0.5) { Frac = 0.25; }
+         else { if (RptTimeFrac < 0.75) { Frac = 0.5; }
+                else { Frac = 0.75; }
+              }
+       }
+  document.Timesheet.ReportTime.value = (Full+Frac).toFixed(2);
+}
 
-?>
-    
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+function inputLimiter(e,allow) {
+  var AllowableCharacters = '';
+  if (allow == 'Numbers'){AllowableCharacters='.1234567890';}
+  var k;
+  k=document.all?parseInt(e.keyCode): parseInt(e.which);
+  if (k!=13 && k!=8 && k!=0){
+    if ((e.ctrlKey==false) && (e.altKey==false)) {
+      return (AllowableCharacters.indexOf(String.fromCharCode(k))!=-1);
+    } else {
+      return true;
+    }
+  } else {
+    return true;
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+</script>
+</div>
   </body>
 </html>
-
-
