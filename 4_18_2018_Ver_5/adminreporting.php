8b661c8d11ccf12fe726dbb958cc5696fdee749f
whitek65 <whitek65@students.rowan.edu>
2018-04-24 11:48:58 -0400

    slight change in button layout

--- 4_18_2018_Ver_5/adminreporting.php
+++ 4_18_2018_Ver_5/adminreporting.php
@@ -1,5 +1,7 @@
 <?php
+
 // Connect to the database
+
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors', 1);
 include('connect.php');
@@ -7,17 +9,13 @@ $dbh = ConnectDB();
 session_start();
 $userid = $_SESSION['userid'];
 $username = $_SESSION['username'];
-
 $sql = "SELECT CONCAT(firstname, ' ' , lastname) AS fullName FROM users where roleId = 2 and active = 1 order BY firstname ASC;";
 $result = mysqli_query($dbh,$sql);
-
 $sql2 = "select datestart from timesheets";
 $result2 = mysqli_query($dbh,$sql2);
 
-
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,7 +27,6 @@ $result2 = mysqli_query($dbh,$sql2);
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 
-
 <body>
     <div class="nav">
                 <img src="https://www.prepsportswear.com/media/images/college_logos/300x300/2126241_mktg_logo.png" class="mainavatar">
@@ -38,21 +35,19 @@ $result2 = mysqli_query($dbh,$sql2);
                 <b><center><font size="6" color="white">Rowan University Field Experience System</font></center></b>
     </div>
 
-
-
-
-
 <div class="navbar">
   <a href="admindashboard.php">Dashboard</a>
-  <div class="dropdown">
-    <button class="dropbtn" onclick="myFunction()">Reports
+        <a href="adminreporting.php">Reporting</a>
+ <div class="dropdown">
+    <button class="dropbtn" onclick="myFunction()">Connections
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content" id="myDropdown">
-        <a href="adminreporting.php">View Timesheets</a>
+        <a href="adminconnections.php">Users</a>
+        <a href="adminfieldsites.php">Field Sites</a>
     </div>
   </div>
-        <a href="adminconnections.php">Connections</a>
+
         <a href="adminprofile.php">Profile</a>
         <a href="login.php" align="right">Logout</a>
 </div>
@@ -61,13 +56,16 @@ $result2 = mysqli_query($dbh,$sql2);
 
 
 
+<form method="post" action="export.php">
+                                <input type="submit" name="export" class="btn btn-success" value="Export All Timesheets" />
+                                </form>
 
 
 
 <div class="form">
 <form action="" method="post">
 <form align="center"  name="Reports">
-<p>Student:
+<p>Select a student:
 <select id='studentname' name='studentname'>
         <?php
         while($row1 = mysqli_fetch_array($result)):;
@@ -75,88 +73,95 @@ $result2 = mysqli_query($dbh,$sql2);
         <option><?php echo $row1[0];?></option>
         <?php endwhile;?>
 </select>
-
-<br />
         <input type="submit" name="submit" value="Search">
-</br>
+
 <center>
+
 <?php
 
 if($_POST['submit']){
-
-
 $studentname = $_POST['studentname'];
-
 $namesplit = explode(" ", $studentname);
-$name = $namesplit[0]; 
-	
-$tbl = "select concat(firstname, ' ', lastname) as name, timesheetsid, unixstamp, total_hours, 
-		coordinatorid, status ";
+$name = $namesplit[0];
+$tbl = "select concat(firstname, ' ', lastname) as name, timesheetsid, unixstamp, total_hours,
+                coordinatorid, status ";
 $tbl .="from rodrigueb6.users ";
 $tbl .= "join rodrigueb6.students s using (userID) ";
 $tbl .= "join rodrigueb6.studenttimesheets sts using (bannerID) ";
 $tbl .= "join rodrigueb6.timesheets ts using (timesheetsID) ";
 $tbl .= "WHERE firstName = '" . $name . "' ";
 
-
-	
 $tblresults = mysqli_query($dbh,$tbl);
-if($tblresults){
-	
-	echo "<table border= '1'>";
-	echo "<tr><th>&nbsp &nbsp Student's Name &nbsp &nbsp </th>
+
+
+
+
+
+if($tblresults ->num_rows > 0){
+        echo "<table border= '1'>";
+        echo "<tr><th>&nbsp &nbsp Student's Name &nbsp &nbsp </th>
         <th>&nbsp &nbsp Timeheet &nbsp &nbsp</th>
         <th>&nbsp &nbsp Date Submitted  &nbsp &nbsp  </th>
         <th>&nbsp &nbsp Total Hours &nbsp &nbsp &nbsp  </th>
         <th>&nbsp &nbsp CoordinatorID  &nbsp  &nbsp  </th>
         <th>&nbsp &nbsp Status &nbsp &nbsp </th>
-		<th>&nbsp &nbsp View Timesheet &nbsp &nbsp </th>
+                <th>&nbsp &nbsp View Timesheet &nbsp &nbsp </th>
                 <th> Select </th></tr>";
-				
-	$tableresult = "";
-while ($row = mysqli_fetch_assoc($tblresults)){
-	
-		$tsidnumber = $row['timesheetid'];
-        
-		$tableresult .= "<tr>";
-		$tableresult .= "<td>" . $row['name'] . "</td>";
-		$tableresult .= "<td>" . $row['timesheetsid'] . "</td>";
-		$tableresult .= "<td>" . $row['unixstamp'] . "</td>";
-		$tableresult .= "<td>" . $row['total_hours'] . "</td>";
-		$tableresult .= "<td>" . $row['coordinatorid'] . "</td>";
-		$tableresult .= "<td>" . $row['status'] . "</td>";
-		$tableresult .= "<td>(<a href='./currenttimesheets.php?timesheetsid=" . $row['timesheetsid'] . "'>View</a>)</td>";
-		$tableresult .= "<td> &nbsp &nbsp &nbsp <input type='checkbox' name='approvebox[".$tsidnumber."]' id='approvebox[$tsidnumber]' value='".$tsidnumber."'/></td>";
-		$tableresult .= "</tr>";
-		}
-		
-		$tableresult .= "</table>";
+        $tableresult = "";
 
-		echo $tableresult;
-		
-		echo "<center>
-		<input style='padding:5px' type='submit' name='approve' class='btn btn-success' value='Approve' />
-		&nbsp &nbsp
-		<input style='padding:5px' type='submit' name='reject' class='btn btn-success' value='Reject' />
-		</center>";
-	}else{
-		echo "tblresults failed. Error: " . mysqli_error($dbh);
-	}
+
+
+if ($row = mysqli_fetch_assoc($tblresults)){
+        $tableresult .= "<br>Timesheets for " . $row['name'] . "<br></br>";
 }
+while ($row = mysqli_fetch_assoc($tblresults)){
+                $tsidnumber = $row['timesheetid'];
+                $tableresult .= "<tr>";
+                $tableresult .= "<td>" . $row['name'] . "</td>";
+                $tableresult .= "<td>" . $row['timesheetsid'] . "</td>";
+                $tableresult .= "<td>" . $row['unixstamp'] . "</td>";
+                $tableresult .= "<td>" . $row['total_hours'] . "</td>";
+                $tableresult .= "<td>" . $row['coordinatorid'] . "</td>";
+                $tableresult .= "<td>" . $row['status'] . "</td>";
+                $tableresult .= "<td>(<a href='./currenttimesheets.php?timesheetsid=" . $row['timesheetsid'] . "'>View</a>)</td>";
+                $tableresult .= "<td> &nbsp &nbsp &nbsp <input type='checkbox' name='approvebox[".$tsidnumber."]' id='approvebox[$tsidnumber]' value='".$tsidnumber."'/></td>";
+                $tableresult .= "</tr>";
+                }
+                $tableresult .= "</table>";
+                echo $tableresult;
+
+                echo "<br>DISPLAY TOTAL HOURS FOR STUDENT HERE </br>";
+
+                echo "<br></br><center>
+                <input style='padding:5px' type='submit' name='approve' class='btn btn-success' value='Approve' />
+                &nbsp &nbsp
+                <input style='padding:5px' type='submit' name='reject' class='btn btn-success' value='Reject' />
+                </center>";
+}
+                else{
+                echo "No timesheets found for student" . mysqli_error($dbh);
+
+        }
+
+}
+
+
+
+
+
+
+
 
 
 if($_POST['approve']){
-	echo "You've pressed approve.\n";
-	print_r($_POST);
-	}
-	
-
+        echo "You've pressed approve.\n";
+        print_r($_POST);
+        }
 if($_POST['reject']){
-	echo "You've pressed reject.";
+        echo "You've pressed reject.";
 }
-
-
 ?>
+
 </center>
 
 <script>
@@ -165,16 +170,11 @@ if($_POST['reject']){
 </form>
 <br />
 
-<br><br></br></br>
-<form method="post" action="export.php">
-                                <input style = "padding:10px" type="submit" name="export" class="btn btn-success" value="Export" />
-                                </form>
 
 </div>
 </form>
-
-
 <script>
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -192,10 +192,5 @@ window.onclick = function(e) {
 }
 </script>
 
-
-
 </div></body>
 </html>
-
-
-
