768720ce39f7288c5356cbef1c99f83bab98a4cc
joned3 <diamondj7@students.rowan.edu>
2018-05-01 12:29:14 -0400

    activate update
    fixed the activate page

--- FinalSeniorProject/activate.php
+++ FinalSeniorProject/activate.php
@@ -1,98 +1,67 @@
 <?php
-
 // Connect to the database
-
 error_reporting(E_ALL ^ E_NOTICE);
-
 ini_set('display_errors', 1);
-
 include('connect.php');
-
-
 $dbh = ConnectDB();
-
-
-
 session_start();
-
 $userid = $_SESSION['userid'];
-
 $username = $_SESSION['username'];
-
-
 ?>
 
 <html>
-
+<html lang="en">
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Activation</title>
-    <link rel="stylesheet" href="style.css"><div class='activate-box'>
-
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Activate Account</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>   		
 </head>
 
-        <body>
+<body style = "background: url(https://www.rowanblog.com/wp-content/uploads/2018/04/Holly-Pointe-roommates-710x335.jpg); background-size: 100% 100%;"
 
-                <form action='' method='post'>
+	<div class="activate-box">
+	<div class="form">
+	<form action='' method='post'>
 
-                <div class='activate-box'>
+	<br></br>	
 
-                <div class='form'>
-
-<?php
-$form = "<img src = 'https://www.prepsportswear.com/media/images/college_logos/300x300/2126241_mktg_logo.png' class='avatar' height='150'>
-        <form class='activate-form'>
-        <h1><center>Activate Account</center></h1>
-        </div>";
-		
+<?php		
 if (!isset($_GET["user"]) && !isset($_GET["code"]) && !$userid == 1){
 	echo "Error: This page is for administrators only. Click <a href='login.php'>here</a> to login.\n";
 }else{
-	
 	echo $form;
-	
 	$user = $_GET["user"];
 	$code = $_GET["code"];
-	
-	$query = "SELECT * FROM users WHERE username = '$user' AND code = '$code'";
+	$query = "SELECT * FROM users WHERE username = '$user' AND code = '$code' limit 1";
 	$result = mysqli_query($dbh, $query);
 	$numrows = mysqli_num_rows($result);
-	if($numrows == 1){
 		$row = mysqli_fetch_assoc($result);
 		$dbuser = $row['username'];
 		$dbfname = $row['firstname'];
 		$dblname = $row['lastname'];
 		$dbpnumber = $row['phone_number'];
 		$dbemail = $row['email'];
-	}
-	else{
-		echo "Error: The specificed username and code either does not exist, or is not unique.\n";
-		echo "Contact your database administrator to determine the proper solution to this problem.\n";
-	}
+	
 }
-
 if ($_POST['submit']){
-        $update = "UPDATE users SET active = 1 WHERE username = '$user' AND code = '$code'";
+        $update = "UPDATE users SET active = 1 WHERE username = '$user' AND code = '$code';";
 		$updateresult = mysqli_query($dbh,$update);
 		if($updateresult){
-			echo "Account has been successfully set to active! Click <a href='admindashboard.php'>here</a> to return to dashboard.\n";
+			echo $update;
+			echo "Account has been successfully set to active! Click <a href='login.php'>here</a> to Log In.\n";
 			$querycheck = "SELECT * FROM users WHERE username = '$user' AND code = '$code'";
 			$resultcheck = mysqli_query($dbh, $querycheck);
 			$rowcheck = mysqli_fetch_assoc($resultcheck);
 			$active = $rowcheck['active'];
-			echo "Current info set: " . $rowcheck['username'] . "\n" . $rowcheck['active'] . "\n";
 		}else{
 			echo "Account update failed; the specified user account has not been set to active. Error: " . mysqli_error($dbh) . "\n";
 		}
-		
 }
 ?>
-
-
+		<h1><center>Activate Account</center></h1>
 		<p>Username: <input type="text" name="uname" value="<?php echo $dbuser; ?>" readonly></p>
         
 		<p>First Name: <input type="text" name="first" value="<?php echo $dbfname; ?>" readonly></p>
@@ -102,9 +71,12 @@ if ($_POST['submit']){
 		<p>Phone Number: <input type="text" name="phone" value="<?php echo $dbpnumber; ?>" readonly></p>
 
 		<p>Email: <input type="text" name="e_mail" value="<?php echo $dbemail; ?>" readonly></p>
-
+ 
         <input type='submit' name='submit' value='Activate'>
-		
-		 </form>
+		</div>
+
+	
+		</form>
         </div>
         </body>
+	</html>	
